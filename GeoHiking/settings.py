c680556e74f27bbb3533d6ffdf1d9f302f462572
PeterP.SVK <peterp.svk@gmail.com>
2017-02-01 12:43:22 +0100

    Problem with GDAL installation on WIN

--- GeoHiking/settings.py
+++ GeoHiking/settings.py
@@ -37,13 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
+    #'GeoHiking.GeoDataDB',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware', # Enable extra checks
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'GeoHiking.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE' : 'django.contrib.gis.db.backends.postgis',
+        'NAME' : 'geodatadb',
+        'USER' : 'geoadmin',
+        'PASSWORD' : 'fds4f4dfs75gf4',
+        'HOST': '192.168.86.128',
+        'PORT': '5432',
     }
 }
 
